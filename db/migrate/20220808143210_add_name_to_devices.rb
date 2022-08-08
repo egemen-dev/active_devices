4ca081847b4d30dbd56df1d056692af010fc18b1
Egemen <egemenwasd@gmail.com>
2022-08-08 18:27:26 +0300

    Add name column to device model

---
+++ db/migrate/20220808143210_add_name_to_devices.rb
@@ -0,0 +1,5 @@
+class AddNameToDevices < ActiveRecord::Migration[7.0]
+  def change
+    add_column :devices, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_08_131143) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_08_143210) do
   create_table "devices", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_08_131143) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "name"
     t.index ["email"], name: "index_devices_on_email"
     t.index ["reset_password_token"], name: "index_devices_on_reset_password_token", unique: true
   end
