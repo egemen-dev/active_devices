ba4542beb3cded18504fe6b9ade010270285405a
Egemen <egemenwasd@gmail.com>
2022-08-09 13:40:27 +0300

    Allow Device login with unique_id

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,7 +2,9 @@ class Device < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :email, uniqueness: false
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
+  validates :unique_id, presence: true, uniqueness: true
+  validates :name, presence: true
+  devise :database_authenticatable, :authentication_keys => [:unique_id]
+  devise :registerable, :recoverable, :rememberable
   belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :devices
   devise_for :users
+  devise_for :devices, controllers: {
+    registrations: "devices/registrations",
+    sessions: "devices/sessions"
+  }
   resources :users, only: [:show]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  get 'devices/all', to: 'devices#all_devices', as: :all_devices
+  get 'devices/:id/new_device', to: 'devices#new_device', as: :new_device
   root "welcome#welcome"
-  
 end
