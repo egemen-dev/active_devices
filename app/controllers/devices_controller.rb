0e2d0ade348c181cecb7eb73e3af8b11482976d9
Egemen <egemenwasd@gmail.com>
2022-08-09 13:44:24 +0300

    Edit new_device method to show information only once

---
+++ app/controllers/devices_controller.rb
@@ -0,0 +1,27 @@
+class DevicesController < ApplicationController
+  before_action :authenticate_user!
+
+  def all_devices
+    @devices = current_user.devices
+  end
+
+  def new_device
+    device = current_user.devices.last
+
+    # Check if sensitive information of the device seen or not
+    if params[:id] == device.id.to_s && device.is_seen == false
+      p new_password = ([*"A".."Z"]+[*"a".."z"]+[*0..9]).shuffle.sample(8).join
+      device.update_attribute("password", new_password)
+      device.update_attribute("is_seen", true)
+      if device.save!
+        p @device_info = {password: new_password, unique_id: device.unique_id, name: device.name} 
+      else
+        flash[:alert] = "Something went wrong, try again."
+        redirect_to all_devices_path
+      end
+    else
+      flash[:alert] = "You don't have a permission to see this device's information!"
+      redirect_to all_devices_path
+    end
+  end
+end
