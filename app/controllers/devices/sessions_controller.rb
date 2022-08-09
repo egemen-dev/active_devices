c42a1f20511c6305ed042621071f5d6990e76ac0
Egemen <egemenwasd@gmail.com>
2022-08-09 13:45:29 +0300

    Update is_active column upon login and logout

--- app/controllers/devices/sessions_controller.rb
+++ app/controllers/devices/sessions_controller.rb
@@ -1,27 +1,38 @@
 # frozen_string_literal: true
 
 class Devices::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
+  after_action :is_active_true, only: [:create]
+  before_action :is_active_false, only: [:destroy]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  end
+
+  def is_active_true
+    device = Device.find_by(unique_id: params[:device][:unique_id])
+    device.update_attribute("is_active", true)
+  end
+  
+  def is_active_false
+    current_device.update_attribute("is_active", false)
+  end
 end
