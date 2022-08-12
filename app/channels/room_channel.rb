b16fc35f1f130c1dba73f6441cd581cbc9f8c6b6
Egemen <egemenwasd@gmail.com>
2022-08-12 21:39:52 +0300

    Demonstrate the broadcasting when connection established and lost

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,24 @@
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "room_channel"
+    online
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+    offline
+  end
+
+  def online
+    ActionCable.server.broadcast "room_channel", { message: current_user.name + ' is now online' }
+  end
+
+  def offline
+    ActionCable.server.broadcast "room_channel", { message: "I'm offline" }
+  end
+
+  def receive(data)
+    ActionCable.server.broadcast("room_channel", "hello")
+    ActionCable.server.broadcast "room_channel", { message: "hello" }
+  end
+end
