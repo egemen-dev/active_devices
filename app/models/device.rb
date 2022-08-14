4fed66ba6a0d49317d73185a03ee1dae68158613
Egemen <egemenwasd@gmail.com>
2022-08-14 23:43:39 +0300

    Add 'broadcasts_to' update partial

--- app/models/device.rb
+++ app/models/device.rb
@@ -7,4 +7,16 @@ class Device < ApplicationRecord
   devise :database_authenticatable, :authentication_keys => [:unique_id]
   devise :registerable, :recoverable, :rememberable
   belongs_to :user
+
+  after_create_commit do
+    broadcast_prepend_later_to("dash_#{self.user_id}", target: "devices", partial: "devices/device", locals: { device: self })
+  end
+
+  after_update_commit do
+    broadcast_replace_to("dash_#{self.user_id}", target: "device_#{self.id}", partial: "devices/device", locals: { device: self })
+  end
+
+  after_destroy_commit do
+    broadcast_remove_to("dash_#{self.user_id}", target: "device_#{self.id}", partial: "devices/device", locals: { device: self })
+  end
 end
